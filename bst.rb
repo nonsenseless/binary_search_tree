2eb811b3323063c65c3e2cb01f894c8ff442cb8d
Sean <sean@nonsenseless.net>
2015-10-28 14:47:04 -0500

    Updated BST to work with unsorted array as well

--- bst.rb
+++ bst.rb
@@ -11,15 +11,34 @@ class Bst
     def to_s
       "Left: #{left.key}\nKey: #{self.key}\nRight: #{right.key}"
     end
+
+    def child_right?
+      return self.right
+    end
+
+    def child_left?
+      return self.left
+    end
+
+    def add_child_left(value)
+      puts "Theroetically, we're adding #{value} to the left of #{self.key}"
+      self.left = Node.new(value)
+    end
+
+    def add_child_right(value)
+      puts "Theoretically, we're adding #{value} to the right of #{self.key}"
+      self.right = Node.new(value)
+    end
+
   end
 
   attr_reader :root
 
-  def initialize(keys)
-    @root = build_tree(keys, 0, (keys.count-1))
+  def initialize(keys, array_is_sorted=false)
+    array_is_sorted ? build_balanced_tree(keys, 0, (keys.count-1)) : build_tree(keys)
   end
 
-  def build_tree(keys, start_idx, end_idx)
+  def build_balanced_tree(keys, start_idx, end_idx)
     #puts "keys at the start of build_tree are #{keys.to_s}"
 
     return nil if start_idx > end_idx
@@ -34,20 +53,34 @@ class Bst
     return root
   end
 
-  def put(key, node=root)
-    if node == nil
-      return Node.new(key)
-    elsif key < node.key
-      put(key, node.left)
+  def build_tree(keys)
+
+
+    @root = Node.new(keys.first)
+    puts "The root of the tree is #{@root.key} from #{keys.first}"
+    keys[1..-1].each do |key|
+      puts "Build tree is putting key #{key}"
+      put(key)
+    end
+
+  end
+
+  def put(key, node=@root)
+    puts "Inside put, key #{key} is being added to node #{node.key}"
+    if key < node.key
+      puts "Put has decided that #{key} is less than #{node.key}"
+      node.child_left? ? put(key, node.left) : node.add_child_left(key)
     elsif key > node.key
-      put(key, node.right)
+      puts "Put has decided that #{key} is greater than #{node.key}"
+      node.child_right? ? put(key, node.right) : node.add_child_right(key)
     else
+      puts "Put has decided that the two keys are the same."
       node.key = key
     end
   end
 
   def to_s
-    puts @root
+    #puts @root
   end
 
 end
